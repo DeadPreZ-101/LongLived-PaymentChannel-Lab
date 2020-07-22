9efb2860fa025a3be1eca5ae26b85976adb20941
DeadPrez-101 <al.chemist.jasat@gmail.com>
2020-07-21 20:10:37 -0400

    Test Added

--- test/longlivepaymentchannel.test..js
+++ test/longlivepaymentchannel.test..js
@@ -6,6 +6,7 @@ contract(
     // declare all global variables here
     let contractInstance;
     let contractAddress;
+    let longLivedPaymentChannelTx; // ******** //
     const skey =
       "dec072ad7e4cf54d8bce9ce5b0d7e95ce8473a35f6ce65ab414faea436a2ee86"; // private key
     web3.eth.accounts.wallet.add(`0x${skey}`);
@@ -15,6 +16,7 @@ contract(
     const recipient = accounts[1];
     const closeDuration = 200;
     const depositAmount = web3.utils.toWei("2", "ether");
+    
     // sender can open the channel (deploy contract and deposit funds)
     before(async () => {
       await web3.eth.sendTransaction({
@@ -24,6 +26,7 @@ contract(
         gas: 21000,
       });
       contractInstance = new web3.eth.Contract(LongLivedPaymentChannel.abi);
+
       const gas = await contractInstance
         .deploy({
           data: LongLivedPaymentChannel.bytecode,
@@ -32,6 +35,108 @@ contract(
           arguments: [recipient, closeDuration],
         })
         .estimateGas();
+    
+      longLivedPaymentChannelTx = await contractInstance
+        .deploy({
+          data: LongLivedPaymentChannel.bytecode,
+          arguments: [recipient, closeDuration],
+        })
+        .send({
+          from: sender,
+          gas,
+          value: depositAmount,
+        });
+      contractAddress = longLivedPaymentChannelTx.options.address;
+      const actualSender = await longLivedPaymentChannelTx.methods
+        .sender()
+        .call({
+          from: recipient,
+        });
+      const actualRecipient = await longLivedPaymentChannelTx.methods
+        .recipient()
+        .call({
+          from: accounts[2],
+        });
+      const actualCloseDuration = await longLivedPaymentChannelTx.methods
+        .closeDuration()
+        .call({
+          from: accounts[2],
+        });
+      const actualDepositedAmount = await web3.eth.getBalance(contractAddress);
+     
+      assert.equal(actualSender, sender, "Sender is not as expected");
+      assert.equal(
+        actualDepositedAmount,
+        depositAmount,
+        "The deposited amount is as expected"
+      );
+      assert.equal(actualRecipient, recipient, "The recipient is as expected");
+      assert.equal(
+        actualCloseDuration,
+        closeDuration,
+        "closeDuration is not as expected"
+      );
+    });
+
+    it("the recipient should be able to withdraw from the channel", async () => {
+      // code that will sign for recipient to withdraw
+      // code that will use this sign as well as recipient as caller of `withdraw` function
+      // the recipient should be able to close the channel
+      // make necessary assertions to validate balance of sender and recipient
+
+      const amountClaimed = web3.utils.toWei("1", "ether");
+      const message = web3.utils.soliditySha3(
+        { t: "address", v: contractAddress },
+        { t: "uint256", v: amountClaimed }
+      );
+
+      const sig = await web3.eth.accounts.sign(message, senderSkey);
+      const realSignature = sig.signature;
+
+      const recBalBefore = await web3.eth.getBalance(recipient);
+
+      const withdrawTx = await longLivedPaymentChannelTx.methods
+        .withdraw(amountClaimed, realSignature)
+        .send({ from: recipient });
+
+      const recBalAfter = await web3.eth.getBalance(recipient);
+
+      const tx = await web3.eth.getTransaction(withdrawTx.transactionHash);
+
+      const transactionFee = web3.utils
+        .toBN(tx.gasPrice)
+        .mul(web3.utils.toBN(withdrawTx.gasUsed));
+
+      const balanceShouldBe = web3.utils
+        .toBN(recBalBefore)
+        .add(web3.utils.toBN(amountClaimed))
+        .sub(web3.utils.toBN(transactionFee));
+
+      assert.equal(
+        balanceShouldBe,
+        recBalAfter,
+        "The balance does not seem to be right"
+      );
+    });
+
+    it("should able to close the payment channel", async () => {
+      await web3.eth.sendTransaction({
+        from: masterAccount,
+        to: sender,
+        value: web3.utils.toWei("5", "ether"),
+        gas: 21000,
+      });
+      contractInstance = new web3.eth.Contract(LongLivedPaymentChannel.abi);
+
+      const gas = await contractInstance
+        .deploy({
+          data: LongLivedPaymentChannel.bytecode,
+          from: sender,
+          value: depositAmount,
+          arguments: [recipient, closeDuration],
+        })
+        .estimateGas();
+
       const longLivedPaymentChannelTx = await contractInstance
         .deploy({
           data: LongLivedPaymentChannel.bytecode,
@@ -43,36 +148,40 @@ contract(
           value: depositAmount,
         });
       contractAddress = longLivedPaymentChannelTx.options.address;
-      const actualSender = await longLivedPaymentChannelTx.methods.sender().call({
-        from: recipient,
-      });
-      const actualRecipient = await longLivedPaymentChannelTx.methods.recipient().call({
-        from:accounts[2]
-      });
-      const actualCloseDuration = await longLivedPaymentChannelTx.methods.closeDuration().call({
-        from:accounts[2]
-      })
-      const actualDepositedAmount = await web3.eth.getBalance(contractAddress);
-      // assertions
-      assert.equal(actualSender, sender, "Sender is not as expected");
-      assert.equal(
-        actualDepositedAmount,
-        depositAmount,
-        "The deposited amount is as expected"
-      );
-      assert.equal(
-        actualRecipient,
-        recipient,
-        "The recipient is as expected"
-      );
-      assert.equal(actualCloseDuration,closeDuration,"closeDuration is not as expected")
-    });
 
-    it("the recipient should be able to withdraw from the channel", async () => {
-      // code that will sign for recipient to withdraw
-      // code that will use this sign as well as recipient as caller of `withdraw` function
-      // the recipient should be able to close the channel
-      // make necessary assertions to validate balance of sender and recipient
+      const amountClaimed = web3.utils.toWei("1", "ether");
+      const message = web3.utils.soliditySha3(
+        { t: "address", v: contractAddress },
+        { t: "uint256", v: amountClaimed }
+      );
+
+      const sig = await web3.eth.accounts.sign(message, senderSkey);
+      const realSignature = sig.signature;
+
+      const recBalBefore = await web3.eth.getBalance(recipient);
+
+      const closeTx = await longLivedPaymentChannelTx.methods
+        .close(amountClaimed, realSignature)
+        .send({ from: recipient });
+
+      const recBalAfter = await web3.eth.getBalance(recipient);
+
+      const tx = await web3.eth.getTransaction(closeTx.transactionHash);
+
+      const transactionFee = web3.utils
+        .toBN(tx.gasPrice)
+        .mul(web3.utils.toBN(closeTx.gasUsed));
+
+      const balanceShouldBe = web3.utils
+        .toBN(recBalBefore)
+        .add(web3.utils.toBN(amountClaimed))
+        .sub(web3.utils.toBN(transactionFee));
+
+      assert.equal(
+        balanceShouldBe,
+        recBalAfter,
+        "The balance does not seem to be right"
+      );
     });
   }
 );
